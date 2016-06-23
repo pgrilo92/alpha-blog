c6d4df3afbcb193f2a7678c016cede5b3a8ee355
Joaquim <pgrilo92@hotmail.com>
2016-06-23 21:46:52 +0000

    modified actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
 
+def index
+    
+    @articles = Article.all
+    
+end
+
+
 def new
     @article = Article.new
 end
+
+def edit
+    @article = Article.find(params[:id])
+    
+    
+end
+
 def create
     
      @article = Article.new(article_params)
@@ -17,9 +31,28 @@ def create
      
 end
 
+def update
+    
+     @article = Article.find(params[:id])
+      if @article.update(article_params
+      )
+     flash[:notice] = "Article was successfully updated"
+     redirect_to article_path(@article)
+    else 
+       render'edit'
+  
+   end
+    
+    
+end
+
+
 def show
     @article = Article.find(params[:id])
 end
+
+
+
 private
 
 def article_params
