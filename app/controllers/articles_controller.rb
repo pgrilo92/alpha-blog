29c338e48ddc4d39141a93b99feae35fa2b3ac11
Joaquim <pgrilo92@hotmail.com>
2016-06-24 20:26:39 +0000

    debug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,8 +20,9 @@ def edit
 end
 
 def create
-    
+
      @article = Article.new(article_params)
+   #  @article.user = User.first
  if @article.save
      flash[:success] = "Article was successfully created"
      redirect_to article_path(@article)
