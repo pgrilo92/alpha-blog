751fb7534f5edd750e2dcfabe7762d7fc65a8013
Joaquim <pgrilo92@hotmail.com>
2016-06-25 17:29:37 +0000

    articles views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
     def new
         @user = User.new
+    end  
+    
+    def index
+    @users= User.all    
     end
+    
     def create
         @user = User.new(user_params)
         if @user.save
@@ -30,7 +35,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         
     end
-    
+  
     
     private
     
