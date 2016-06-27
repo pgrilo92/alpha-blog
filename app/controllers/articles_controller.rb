eaf3c0a51e8064fb027d788f4990bef294da319a
Joaquim <pgrilo92@hotmail.com>
2016-06-27 01:58:29 +0000

    typo2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,7 +32,7 @@ def create
     else 
        render'new'
   
-   end
+ end
     
      
 end
@@ -43,10 +43,10 @@ def update
       if @article.update(article_params)
      flash[:success] = "Article was successfully updated"
      redirect_to article_path(@article)
-    else 
-       render'edit'
+            else 
+                render'edit'
   
-   end
+      end
     
     
 end
