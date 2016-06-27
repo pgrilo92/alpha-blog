6f8844df24af958f93fc2b3bafcce0b038852ed3
Joaquim <pgrilo92@hotmail.com>
2016-06-27 01:23:31 +0000

    typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
    
    before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
-  before_action :require_admin, ony: [:destroy]
+  before_action :require_admin, only: [:destroy]
     def new
         @user = User.new
     end  
