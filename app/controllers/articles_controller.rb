586ac763899775e2ea74a78da185bb2843777720
Joaquim <pgrilo92@hotmail.com>
2016-06-28 15:49:43 +0000

    category editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -72,7 +72,7 @@ def set_article
 end
 
 def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
 end
 def require_same_user
     if current_user != @article.user and !current_user.admin?
